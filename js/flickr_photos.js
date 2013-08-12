919ac1a32bb033fa367bc173dd819f00863c63a5
Victor Pascual <victor.pascual@gmail.com>
2013-08-12 14:19:13 +0200

    Solved issue#1 and added more tests

--- js/flickr_photos.js
+++ js/flickr_photos.js
@@ -1,7 +1,11 @@
 var constants = {
 		'main_url' : 'url_l',
 		'num_photos_per_page' : 500,
-		'default_granularity' : 1
+		'default_granularity' : 1,
+		'hours' : 0,
+		'days' : 1,
+		'weeks' : 2,
+		'months' : 3
 	}
 	/*
 		0 - Hours
@@ -12,18 +16,26 @@ var constants = {
 	var granularity; 
 	var initDate;
 	var endDate;
-	var photos = [];
-	var currentPhoto = 0; // stores the number of photos that have been processed
-	var histogram = []; // each position of this array will contain a sorted array of the pictures that belong to each bin	
+	var photos;
+	var currentPhoto; // stores the number of photos that have been processed
+	var histogram; // each position of this array will contain a sorted array of the pictures that belong to each bin	
 	var callbackOnFinish;
+	var userId;
 	
-	function init(photoInitDate, photoEndDate, callback, binsGranularity) {
+	function init(userId, photoInitDate, photoEndDate, callback, binsGranularity) {		
+		currentPhoto = 0;
+		photos = [];
+		userId = this.userId;
 		initDate = photoInitDate;
 		endDate = photoEndDate;
 		granularity = (binsGranularity == undefined) ? constants.default_granularity : binsGranularity;
 		callbackOnFinish = callback;
 		searchPhotos(1, onPageLoaded);		
-	}	
+	}
+
+	function setGranularity(value) {
+		granularity = value;
+	}
 
 	function onPageLoaded(data) {
 		console.log(photos)
@@ -43,6 +55,9 @@ var constants = {
 	    		console.log("Date: " + photo.datetaken)
 	    		photo.date = flickrDateToDate(photo.datetaken);	    		
 	    	}
+	    	photos.sort(function(a,b) {
+	    		return a.date - b.date;
+	    	});
 	    	getDataStructure();
 	    }
 	}
@@ -54,11 +69,11 @@ var constants = {
 			    	//oauth_nonce : 89601180, 
 			    	method : 'flickr.photos.search',
 			    	api_key : '4aa644ee462a328d852a34e4a0fe7855',
-			    	user_id : '12001399@N05',
+			    	user_id : userId,
 			    	min_taken_date : initDate,
 			    	max_taken_date : endDate,
 			    	per_page : constants.num_photos_per_page,
-			    	sort : 'date-posted-asc',
+			    	sort : 'date-taken-asc',
 			    	extras : 'date_taken, original_format, geo, date_taken, original_format, geo, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o',
 			    	page : numPage,
 			    	format : 'json',
@@ -71,27 +86,31 @@ var constants = {
  		var splitDate = datetaken.split(' ');
     	day = splitDate[0].split('-');
     	hour = splitDate[1].split(':');
-    	date = new Date(day[0], day[1], day[2], hour[0], hour[1], hour[2]);
+    	// months start at 0, so Febrruary is month 1
+    	date = new Date(day[0], parseInt(day[1])-1, day[2], hour[0], hour[1], hour[2]);
     	return date;
  	}
 
- 	function getDataStructure() { 		
+ 	function getDataStructure() { 	
+ 		histogram = [];	
  		var currentArray = [];
- 		var lastBinIdentifier = getGranularityValue(photos[0].date);
+ 		var lastDate = getTimeValue(photos[0].date);
 
  		for (var i = 0; i<photos.length; i++) {
- 			currentPhotoIdentifier = getGranularityValue(photos[i].date)
+ 			currentDateValue = getTimeValue(photos[i].date)
  			
- 			if (currentPhotoIdentifier == lastBinIdentifier) {
- 				console.log("Adding photo " + photos[i].id + " with date " + photos[i].date + " to bin " + currentPhotoIdentifier)
+ 			if (currentDateValue == lastDate) {
+ 				console.log("Adding photo " + photos[i].id + " with date " + photos[i].date + " to bin " + currentDateValue)
  				currentArray.push(photos[i]);
  				console.log("Current array has now " + currentArray.length + " values")
- 			} else { 				
- 				console.log("Found a new identifier " + currentPhotoIdentifier + " different from " + lastBinIdentifier);
+ 			} else {
+ 				// first array always go to first position without filling gaps
+ 				console.log("Found a new identifier " + currentDateValue + " different from " + lastDate);
  				histogram.push(currentArray);
- 				console.log("Histgraom has now " + histogram.length + " values");
- 				lastBinIdentifier = currentPhotoIdentifier;
- 				console.log("Adding photo " + photos[i].id + " with date " + photos[i].date + " to bin " + currentPhotoIdentifier)
+ 				fillGaps(lastDate, currentDateValue);
+ 				console.log("Histgram has now " + histogram.length + " values");
+ 				lastDate = currentDateValue;
+ 				console.log("Adding photo " + photos[i].id + " with date " + photos[i].date + " to bin " + currentDateValue)
  				currentArray = [];
  				currentArray.push(photos[i]);
  			}	
@@ -102,28 +121,44 @@ var constants = {
  	}
 
  	/**
+ 	* This functions fills the "histogram" with empty arrays if there are some hours/days/weeks/months with no pictures
+ 	**/
+ 	function fillGaps(lastDate, currentDateValue) {
+ 		console.log("Filling gaps between " + lastDate + " and " + currentDateValue);
+ 		diff = currentDateValue - lastDate;
+
+ 		for (var i = 1; i<diff; i++) {
+ 			console.log("Added one gap");
+ 			histogram.push([]);
+ 		}
+ 	}
+
+ 	/**
  	*	This could also be solved with the granularity param: http://www.flickr.com/services/api/misc.dates.html
  	* 	Nevertheless this param doesn't allow the "week" granularity so decided to do it by myself creating the  	"date!" parameter in the "photo" object
  	**/
- 	function getGranularityValue(date) {
+ 	function getTimeValue(date) {
+ 		tmpDate = new Date();
+ 		tmpDate.setTime(date.getTime());
+ 		// set minutes, seconds and milliseconds to 0
+ 		tmpDate.setMinutes(0);
+		tmpDate.setSeconds(0);
+		tmpDate.setMilliseconds(0);
+
  		switch(granularity)
-			{
-				case 0: // hours
-					console.log("Current granularity value: " + String(date.getMonth) + String(date.getDay()) + date.getHours());
-					return String(date.getMonth) + String(date.getDay()) + String(date.getHours());
-				case 1: // days
-					console.log("Current granularity value: " + String(date.getMonth()) + String(date.getDay()));
-					return String(date.getMonth()) + String(date.getDay());
-				case 2: // weeks
-					console.log("Current granularity value: " + getWeekNumber(date));
-					return getWeekNumber(date);
-				case 3: // months
-					console.log("Current granularity value: " + date.getYear() + date.getMonth());
-					return date.getYear() + date.getMonth();
-				default:
-					console.log("Return default granularity value: " + String(date.getMonth()) + String(date.getDay()));
-					return String(date.getMonth()) + String(date.getDay());
-			}
+		{
+			case 0: // hours					
+				return tmpDate.getTime() / 3600000;
+			case 1: // days
+				tmpDate.setHours(0);
+				return tmpDate.getTime() / 86400000;
+			case 2: // weeks
+				return Math.floor(tmpDate.getTime() / 604800000);
+			case 3: // months
+				return tmpDate.getFullYear() * 12 + tmpDate.getMonth();
+			default:
+				return tmpDate.getTime() / 86400000; // days
+		}
  	}
 
  	/*
